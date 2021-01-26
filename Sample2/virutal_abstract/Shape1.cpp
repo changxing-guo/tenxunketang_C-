e83c3973902f8e62433d819960233238de499e27
changxing-guo <1782451600@qq.com>
2021-01-26 22:55:19 +0800

    纯虚函数和抽象类

---
+++ Sample2/virutal_abstract/Shape1.cpp
@@ -0,0 +1,51 @@
+﻿#include "Shape1.h"
+#include <iostream>
+
+using namespace std;
+
+Shape1::Shape1()
+{
+	cout << "构造Shape1" << endl;
+}
+
+
+Shape1::~Shape1()
+{
+	cout << "析构Shape1" << endl;
+}
+
+test1::test1()
+{
+	cout << "构造test1" << endl;
+}
+
+double test1::getArea()
+{
+	cout << "test1::getArea" << endl;
+	return 0.0;
+}
+
+test1::~test1()
+{
+	cout << "析构test1" << endl;
+}
+
+test2::test2()
+{
+	cout << "析构test2" << endl;
+}
+
+double test2::getArea()
+{
+	cout << "test2::getArea" << endl;
+	return 0.0;
+}
+
+void test2::testVirtual()
+{
+}
+
+test2::~test2()
+{
+	cout << "析构test2" << endl;
+}
---
+++ Sample2/virutal_abstract/Shape1.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+class Shape1
+{
+public:
+	Shape1();
+	virtual double getArea() = 0;
+	virtual ~Shape1();
+
+};
+
+class test1 : public Shape1
+{
+public:
+	test1();
+	double getArea();
+	virtual void testVirtual() = 0;
+	~test1();
+
+protected:
+	int x_;
+	int y_;
+};
+
+class test2 :public test1
+{
+public:
+	test2();
+	double getArea();
+	void testVirtual();
+	~test2();
+protected:
+	int x_;
+	int y_;
+};
+
